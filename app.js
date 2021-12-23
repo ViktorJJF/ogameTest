d74490fe95dcc2f06c480c1e233acce6470effdc
ViktorJJF <vj.jimenez96@gmail.com>
2021-12-22 22:20:39 -0500

    test

--- app.js
+++ app.js
@@ -17,40 +17,40 @@
   await page.waitForSelector(".cookiebanner5:nth-child(2)");
   await page.click(".cookiebanner5:nth-child(2)");
 
-  await page.waitForSelector(
-    ".loginRegister > #loginRegisterTabs > .tabsList > li:nth-child(1) > span"
-  );
-  await page.click(
-    ".loginRegister > #loginRegisterTabs > .tabsList > li:nth-child(1) > span"
-  );
+  // await page.waitForSelector(
+  //   ".loginRegister > #loginRegisterTabs > .tabsList > li:nth-child(1) > span"
+  // );
+  // await page.click(
+  //   ".loginRegister > #loginRegisterTabs > .tabsList > li:nth-child(1) > span"
+  // );
 
-  await page.waitForSelector("#loginTab");
-  await page.click("#loginTab");
+  // await page.waitForSelector("#loginTab");
+  // await page.click("#loginTab");
 
   await page.waitForSelector('input[type="email"]');
   await page.click('input[type="email"]');
-  await page.type('input[type="email"]', "rodrigo.diazranilla@gmail.com", {
+  await page.type('input[type="email"]', "smartsis.ia@gmail.com", {
     delay: 10,
   });
 
   await page.waitForSelector('input[type="password"]');
   await page.click('input[type="password"]');
-  await page.type('input[type="password"]', "phoneypeople", { delay: 10 });
+  await page.type('input[type="password"]', "MyLittleSecret$87", { delay: 10 });
   // await page.click("input[type='checkbox']");
 
   // await page.waitForSelector(
   //   "#loginForm > p > button.button.button-primary.button-lg"
   // );
   console.log("antes de click");
-  await click(page, ".button-facebook");
-  // await click(page, "#loginForm > p > button.button.button-primary.button-lg");
-  // await page.evaluate(() => {
-  //   console.log(
-  //     "el elemento: ",
-  //     document.querySelector("button[type='submit']")
-  //   );
-  //   return document.querySelector("button[type='submit']").click();
-  // });
+  // await click(page, ".button-facebook");
+  await click(page, "#loginForm > p > button.button.button-primary.button-lg");
+  await page.evaluate(() => {
+    console.log(
+      "el elemento: ",
+      document.querySelector("button[type='submit']")
+    );
+    return document.querySelector("button[type='submit']").click();
+  });
   console.log("despues de click");
   await page.screenshot({
     path: "screen9.png",
