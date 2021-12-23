d164969ba59740e14a0b24673001a76a1ecd4f68
ViktorJJF <vj.jimenez96@gmail.com>
2021-12-22 19:19:36 -0500

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (async () => {
   const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch({
-    devtools: true,
+    dumpio: true
     headless: true,
     // executablePath: "/usr/bin/firefox",
     args: [
@@ -44,14 +44,15 @@
   console.log("antes de click");
   await page.click("#loginForm > p > button.button.button-primary.button-lg");
   await page.evaluate(() => {
-    document.querySelector("button[type='submit']").click();
+    console.log("el elemento: ",document.querySelector("button[type='submit']"));
+    return document.querySelector("button[type='submit']").click();
   });
   console.log("despues de click");
   //   await page.screenshot({
   //     path: __dirname + "/screens/screen9.png",
   //   });
   await page.waitForSelector("div > #joinGame > a > .button > span", {
-    timeout: 15000,
+    timeout: 10000,
   });
   console.log("hecho!!");
   //   await browser.close();
