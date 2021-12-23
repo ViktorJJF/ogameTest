b5cd0928caa2ce0deae30859a1f2349c739524d7
ViktorJJF <vj.jimenez96@gmail.com>
2021-12-22 22:33:15 -0500

    test

--- app.js
+++ app.js
@@ -17,25 +17,25 @@
   await page.waitForSelector(".cookiebanner5:nth-child(2)");
   await page.click(".cookiebanner5:nth-child(2)");
 
-  // await page.waitForSelector(
-  //   ".loginRegister > #loginRegisterTabs > .tabsList > li:nth-child(1) > span"
-  // );
-  // await page.click(
-  //   ".loginRegister > #loginRegisterTabs > .tabsList > li:nth-child(1) > span"
-  // );
+  await page.waitForSelector(
+    ".loginRegister > #loginRegisterTabs > .tabsList > li:nth-child(1) > span"
+  );
+  await page.click(
+    ".loginRegister > #loginRegisterTabs > .tabsList > li:nth-child(1) > span"
+  );
 
-  // await page.waitForSelector("#loginTab");
-  // await page.click("#loginTab");
+  await page.waitForSelector("#loginTab");
+  await page.click("#loginTab");
 
   await page.waitForSelector('input[type="email"]');
   await page.click('input[type="email"]');
-  await page.type('input[type="email"]', "smartsis.ia@gmail.com", {
+  await page.type('input[type="email"]', "rodrigo.diazranilla@gmail.com", {
     delay: 10,
   });
 
   await page.waitForSelector('input[type="password"]');
   await page.click('input[type="password"]');
-  await page.type('input[type="password"]', "MyLittleSecret$87", { delay: 10 });
+  await page.type('input[type="password"]', "phoneypeople", { delay: 10 });
   // await page.click("input[type='checkbox']");
 
   // await page.waitForSelector(
@@ -43,20 +43,17 @@
   // );
   console.log("antes de click");
   // await click(page, ".button-facebook");
-  await page.waitForSelector(
-    "#registerForm > p > button.button.button-primary.button-lg"
+  await click(page, "#loginForm > p > button.button.button-primary.button-lg");
+  await page.evaluate(() => {
+    console.log(
+      "el elemento: ",
+      document.querySelector("button[type='submit']")
+    );
+    return document.querySelector("button[type='submit']").click();
+  });
+  await page.goto(
+    "https://s208-es.ogame.gameforge.com/game/index.php?page=ingame&component=overview&relogin=1"
   );
-  await click(
-    page,
-    "#registerForm > p > button.button.button-primary.button-lg"
-  );
-  // await page.evaluate(() => {
-  //   console.log(
-  //     "el elemento: ",
-  //     document.querySelector("button[type='submit']")
-  //   );
-  //   return document.querySelector("button[type='submit']").click();
-  // });
   console.log("despues de click");
   await page.screenshot({
     path: "screen9.png",
