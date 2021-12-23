9e8310a22b0811949de1bdd475807e1bf6ddec0c
ViktorJJF <vj.jimenez96@gmail.com>
2021-12-22 22:13:12 -0500

    test

--- app.js
+++ app.js
@@ -42,6 +42,7 @@
   //   "#loginForm > p > button.button.button-primary.button-lg"
   // );
   console.log("antes de click");
+  await click(page, ".button-facebook");
   await click(page, "#loginForm > p > button.button.button-primary.button-lg");
   await page.evaluate(() => {
     console.log(
