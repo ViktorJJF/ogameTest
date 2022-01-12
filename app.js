f26c35271d0984999fd81efbb4753cbbc8f70790
ViktorJJF <vj.jimenez96@gmail.com>
2022-01-11 19:36:41 -0500

    mas logs

--- app.js
+++ app.js
@@ -71,80 +71,80 @@
     //   page,
     //   browser
     // );
-    var data = JSON.stringify({
-      identity: "viktor.developer96@gmail.com",
-      password: "sed4cfv52309$",
-      locale: "en_GB",
-      gfLang: "en",
-      platformGameId: "1dfd8e7e-6e1a-4eb1-8c64-03c3b62efd2f",
-      gameEnvironmentId: "0a31d605-ffaf-43e7-aa02-d06df7116fc8",
-      autoGameAccountCreation: false,
-    });
+    // var data = JSON.stringify({
+    //   identity: "viktor.developer96@gmail.com",
+    //   password: "sed4cfv52309$",
+    //   locale: "en_GB",
+    //   gfLang: "en",
+    //   platformGameId: "1dfd8e7e-6e1a-4eb1-8c64-03c3b62efd2f",
+    //   gameEnvironmentId: "0a31d605-ffaf-43e7-aa02-d06df7116fc8",
+    //   autoGameAccountCreation: false,
+    // });
 
-    var config = {
-      method: "post",
-      url: "https://gameforge.com/api/v1/auth/thin/sessions",
-      headers: {
-        authority: "gameforge.com",
-        "sec-ch-ua":
-          '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-        "content-type": "application/json",
-        "tnt-installation-id": "",
-        "sec-ch-ua-mobile": "?0",
-        "user-agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
-        "sec-ch-ua-platform": '"Windows"',
-        accept: "*/*",
-        origin: "https://lobby.ogame.gameforge.com",
-        "sec-fetch-site": "same-site",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-dest": "empty",
-        referer: "https://lobby.ogame.gameforge.com/",
-        "accept-language": "en",
-      },
-      data: data,
-    };
+    // var config = {
+    //   method: "post",
+    //   url: "https://gameforge.com/api/v1/auth/thin/sessions",
+    //   headers: {
+    //     authority: "gameforge.com",
+    //     "sec-ch-ua":
+    //       '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+    //     "content-type": "application/json",
+    //     "tnt-installation-id": "",
+    //     "sec-ch-ua-mobile": "?0",
+    //     "user-agent":
+    //       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+    //     "sec-ch-ua-platform": '"Windows"',
+    //     accept: "*/*",
+    //     origin: "https://lobby.ogame.gameforge.com",
+    //     "sec-fetch-site": "same-site",
+    //     "sec-fetch-mode": "cors",
+    //     "sec-fetch-dest": "empty",
+    //     referer: "https://lobby.ogame.gameforge.com/",
+    //     "accept-language": "en",
+    //   },
+    //   data: data,
+    // };
 
-    const response = await axios(config);
-    const token = response.data.token;
-    // const token = "39dc589f-9027-4824-810e-2897412dd2f9";
-    console.log("🚀 Aqui *** -> token", token);
-    await page.evaluate((token) => {
-      console.log("el token: ", token);
-      function setCookie(name, value, days) {
-        var expires = "";
-        if (days) {
-          var date = new Date();
-          date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-          expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + (value || "") + expires + "; path=/";
-      }
-      function getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(";");
-        for (var i = 0; i < ca.length; i++) {
-          var c = ca[i];
-          while (c.charAt(0) == " ") c = c.substring(1, c.length);
-          if (c.indexOf(nameEQ) == 0)
-            return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-      }
-      function eraseCookie(name) {
-        document.cookie =
-          name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-      }
-      setCookie("gf-token-production", token, 7);
-      console.log("COOKIE AGREGADO!");
-      return true;
-    }, token);
-    await page.goto(
-      "https://s208-es.ogame.gameforge.com/game/index.php?page=ingame&component=overview&relogin=1"
-    );
-    await page.goto(
-      "https://s208-es.ogame.gameforge.com/game/index.php?page=ingame&component=overview&relogin=1"
-    );
+    // const response = await axios(config);
+    // const token = response.data.token;
+    // // const token = "39dc589f-9027-4824-810e-2897412dd2f9";
+    // console.log("🚀 Aqui *** -> token", token);
+    // await page.evaluate((token) => {
+    //   console.log("el token: ", token);
+    //   function setCookie(name, value, days) {
+    //     var expires = "";
+    //     if (days) {
+    //       var date = new Date();
+    //       date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    //       expires = "; expires=" + date.toUTCString();
+    //     }
+    //     document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    //   }
+    //   function getCookie(name) {
+    //     var nameEQ = name + "=";
+    //     var ca = document.cookie.split(";");
+    //     for (var i = 0; i < ca.length; i++) {
+    //       var c = ca[i];
+    //       while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    //       if (c.indexOf(nameEQ) == 0)
+    //         return c.substring(nameEQ.length, c.length);
+    //     }
+    //     return null;
+    //   }
+    //   function eraseCookie(name) {
+    //     document.cookie =
+    //       name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    //   }
+    //   setCookie("gf-token-production", token, 7);
+    //   console.log("COOKIE AGREGADO!");
+    //   return true;
+    // }, token);
+    // await page.goto(
+    //   "https://s208-es.ogame.gameforge.com/game/index.php?page=ingame&component=overview&relogin=1"
+    // );
+    // await page.goto(
+    //   "https://s208-es.ogame.gameforge.com/game/index.php?page=ingame&component=overview&relogin=1"
+    // );
     await page.waitForSelector("div > #joinGame > a > .button > span", {
       timeout: 10000,
     });
